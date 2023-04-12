0c7d84dbdd46099d07712932d4c0a8bc1c6c4dad
Diego Naranjo <contact@diegonaranjo.co>
2023-04-11 19:42:21 -0500

    clase29 parte5 front

--- clase29/src/index.js
+++ clase29/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const router = require('./router')
 const mongoConect = require('../db')
 
@@ -6,6 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 router(app)
 
